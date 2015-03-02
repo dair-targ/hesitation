faec718732a697ef18aab4dcd7a85d73dd50279e
Vladimir Berkutov <vladimir.berkutov@gmail.com>
2015-03-03 00:22:52 +0300

    Add Fundamental Frequency calculation

--- japan99.py
+++ japan99.py
@@ -4,15 +4,19 @@ import logging
 import optparse
 import math
 import numpy
-from matplotlib import mlab
+from matplotlib import mlab, pyplot
 from librosa import core
-
+from scipy import signal, optimize
+from scipy.stats import norm
 
 REF_Hz = 440.0 * 2**(3.0 / 12.0 - 5.0)
 
 
 def f_cent(f_hz):
     return 1200.0 * math.log(f_hz / REF_Hz, 2.0)
+
+def cent_to_hz(in_cent):
+    return 2.0**(in_cent / 1200.0) * REF_Hz
 
 
 def G(x, m, sigma):
@@ -50,25 +54,48 @@ def main():
     instantaneous_frequencies = numpy.transpose(instantaneous_frequencies)
     short_time_fourier_transform = numpy.transpose(short_time_fourier_transform)
 
+    logging.info('Computing p(x, F)...')
+    pXF = numpy.ndarray(shape=(len(frequencies), len(frequencies)), dtype='float64')
+    for ix, x in enumerate(frequencies):
+        for iF, F in enumerate(frequencies):
+            pXF[ix][iF] = p(frequencies[ix], frequencies[iF])
+
     logging.info('Estimating Fundamental Frequency...')
     F0s = []
     delta = instantaneous_frequencies - frequencies
-    for t, midpoint in enumerate(midpoints):
+    for t, midpoint in enumerate(midpoints[:10]):
         logging.info('Calculating for %f', midpoint)
         stable_frequencies = []
+        Psi_p = short_time_fourier_transform[t]
         delta_t = delta[t]
         for k in xrange(len(delta_t) - 1):
-            if delta_t[k] > 0 and delta_t[k + 1] < 0:
-                stable_frequencies.append((k, delta_t[k]))
-        best_F = frequencies[1]
-        best_P0 = P_F0(best_F, short_time_fourier_transform[t], stable_frequencies)
-        for iF, F in enumerate(frequencies[2:]):
-            candidate_P_F0 = P_F0(F, short_time_fourier_transform[t], stable_frequencies)
+            if delta_t[k] > 0 > delta_t[k + 1]:
+                stable_frequencies.append(k)
+        P_F0 = lambda iF: sum(pXF[ix][iF] * Psi_p[ix] for ix in stable_frequencies)
+
+        best_iF = 1
+        best_P0 = P_F0(best_iF)
+        for iF in xrange(len(frequencies[2:])):
+            candidate_P_F0 = P_F0(iF)
             if candidate_P_F0 > best_P0:
-                best_F = F
+                best_iF = iF
                 best_P0 = candidate_P_F0
-        F0s.append(best_F)
-    logging.info('Done')
+        F0s.append(frequencies[best_iF])
+    # Suppose F0s are calculated correctly...
+
+    logging.info('Estimating Spectral Envelope...')
+    pyplot.axvline(f_cent(F0s[9]))
+    x = cent_frequencies
+    y = spectrum[10]
+    norm.fit_loc_scale()
+    pyplot.plot(x, y)
+    pyplot.show()
+    exit()
+
+    logging.info('Obtaining Two Features of Filled Pauses...')
+    pyplot.plot(frequencies, short_time_fourier_transform[10])
+    pyplot.show()
+    exit()
 
 
 
